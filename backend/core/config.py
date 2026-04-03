4dbeebffdb4ac378d0a69a3d645238452e14fe96
imsg1006 <shlokgupta10@gmail.com>
2026-04-03 19:53:17 +0530

    changes the sqlalchemy

--- backend/core/config.py
+++ backend/core/config.py
@@ -26,3 +26,6 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+if settings.DATABASE_URL and settings.DATABASE_URL.startswith("postgres://"):
+    settings.DATABASE_URL = settings.DATABASE_URL.replace("postgres://", "postgresql://", 1)
